48bf977439669fdc8d4e67755098f373e571f45a
codereverma <80462262+codereverma@users.noreply.github.com>
2021-10-01 12:54:13 +0530

    NymphoCat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,30 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+  "env": {
+      "browser": true,
+      "es6": true,
+      "node": true
   },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@withfig/fig-linter/recommended",
-    "plugin:compat/recommended",
-  ],
-  env: {
-    browser: true,
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "sourceType": "module"
   },
-  plugins: ["@withfig/fig-linter"],
-  rules: {
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "no-unused-vars": ["off"],
-    "no-var": ["off"],
-    "@typescript-eslint/no-unused-vars": ["off"],
-  },
+  "rules": {
+      "indent": [
+          "error",
+          4
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "no-console": 0
+  }
 };
